26f4fea0124d4b7d2934504f3abac4f3c9b990e5
saisusaku <yusuf.hassanat@gmail.com>
2026-09-22 11:55:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -282,5 +282,8 @@ def add_header(response):
         
     return response
 
+import os
+
 if __name__ == '__main__':
-    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host='0.0.0.0', port=port)
